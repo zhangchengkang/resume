5cb70d64dafae1cf564e4fb363833ecb3ba7f8d0
zhangchengkang <952538291@qq.com>
2019-02-28 23:18:27 +0800

    打包代码

--- src/main/java/com/resume/ResumeApplication.java
+++ src/main/java/com/resume/ResumeApplication.java
@@ -2,12 +2,21 @@ package com.resume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ResumeApplication {
+public class ResumeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ResumeApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(ResumeApplication.class);
+    }
+
 
 }
